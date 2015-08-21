2d91e30477acfa506f2c875a56b0fbfea43b46c5
Sarvesh Ahuja <sarvesh.ahuja@salesforce.com>
2015-08-21 11:48:36 -0400

    adding files

---
+++ README.md
@@ -0,0 +1,13 @@
+# PHP Demo Button
+
+Create by Sarvesh Ahuja 
+
+
+## Deploy to Heroku
+
+You can deploy this app to Heroku and have it up and running instantly.
+
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+
+
---
+++ init/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+  
+  <head>
+    <title>Salesforce Marketing Cloud - Welcome to the Fuel Platform</title>
+  </head>
+  
+  <body>
+    <table width="100%">
+      <tr>
+        <td bgcolor="#7C868D" height="150" width="100%">
+          <table align="center" width="100%">
+            <tr>
+              <td align="left"><img src="http://image.s6.exacttarget.com/lib/fe9112727360007b77/m/1/SFMC_logo.png" style="width: 50%;"></td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+      <tr>
+        <td bgcolor="#E98300" height="60" width="100%"></td>
+      </tr>
+    </table>
+    
+    <table width="60%" align="center">
+      <tr>
+        <td align="center"><br>
+          <h1>Welcome to the Fuel Platform</h1>
+        </td>
+      </tr>
+      
+      <tr>
+        <td align="center">This application contains Salesforce Marketing Cloud's Fuel PHP SDK, and provides seamless access to our API services (including REST and SOAP).</td>
+      </tr>
+      <br>
+      <tr>
+        <td align="center">Learn more about our Fuel SDKs along with documentation and examples:</td>
+      </tr>
+      
+      <tr>
+        <td align="center"><a target="_blank" href="https://code.exacttarget.com/apis-sdks/fuel-sdks/">Salesforce Marketing Cloud's Fuel SDKs</a></td>
+      </tr>
+      
+      <tr>
+        <td align="center"><a target="_blank" href="https://github.com/salesforcefuel/FuelSDK-PHP.git">Fuel PHP SDK - Github Repository</a></td>
+      </tr>
+      <br>
+      <tr>
+        <td align="center"><br>For step-by-step instructions on creating a Salesforce Marketing Cloud's ClientID/ClientSecret and API Key, you can check out our code@ website:</td>
+      </tr>
+      
+      <tr>
+        <td align="center"><a target="_blank" href="http://code.exacttarget.com/apis-sdks/rest-api/using-app-center-to-get-an-api-key.html">Salesforce Marketing Cloud - App Center</a></td>
+      </tr>
+    </table>
+  </body>
+</html>
