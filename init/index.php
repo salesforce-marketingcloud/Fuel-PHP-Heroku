c4d6db72159985ba79a51f1579f16075eecd107f
Sarvesh Ahuja <sarvesh.ahuja@salesforce.com>
2015-08-21 12:02:58 -0400

    updating webpage

--- init/index.php
+++ init/index.php
@@ -9,7 +9,7 @@
     <table width="100%">
       <tr>
         <td bgcolor="#7C868D" height="150" width="100%">
-          <table align="center" width="100%">
+          <table align="center" width="75%">
             <tr>
               <td align="left"><img src="http://image.s6.exacttarget.com/lib/fe9112727360007b77/m/1/SFMC_logo.png" style="width: 50%;"></td>
             </tr>
@@ -31,6 +31,7 @@
       <tr>
         <td align="center">This application contains Salesforce Marketing Cloud's Fuel PHP SDK, and provides seamless access to our API services (including REST and SOAP).</td>
       </tr>
+      <br>
       <br>
       <tr>
         <td align="center">Learn more about our Fuel SDKs along with documentation and examples:</td>
